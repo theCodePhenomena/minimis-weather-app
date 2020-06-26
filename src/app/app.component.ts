cd6540c323274a0c8cfbad6b5ee56c28aeede536
Braga Ion Raducu <radu.braga@hogarthww.com>
2020-06-26 22:15:49 +0300

    Create layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'minimis';
+  showMenu: boolean = false;
+  darkModeActive: boolean = false;
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  toggleMode() {
+    this.darkModeActive = !this.darkModeActive;
+  }
 }
