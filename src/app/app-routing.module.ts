c11235a3d5cfc6fb7cffbf4ae306d577c9dbf9aa
Braga Ion Raducu <radu.braga@hogarthww.com>
2020-06-29 10:31:29 +0300

    Create routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'add', component: AddCardComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
