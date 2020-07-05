186e9ad4a475a0c306b90595f1d57f90358135fe
Braga Ion Raducu <radu.braga@hogarthww.com>
2020-07-05 11:51:33 +0300

    Add route to details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddCardComponent } from './add-card/add-card.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'add', component: AddCardComponent},
+  {path: 'details/:city', component: DetailsComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: HomeComponent}
 ];
