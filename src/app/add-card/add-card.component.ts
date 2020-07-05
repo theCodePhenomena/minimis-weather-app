5a79556d9af55f213018fff72fb5c915d28d2297
Braga Ion Raducu <radu.braga@hogarthww.com>
2020-07-05 10:53:24 +0300

    Create add card

--- src/app/add-card/add-card.component.ts
+++ src/app/add-card/add-card.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-card.component.scss']
 })
 export class AddCardComponent implements OnInit {
+  darkMode = false;
 
   constructor() { }
 
