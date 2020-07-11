73763dc4428aa6d835bfd203c40b5b62ea61fc4a
Braga Ion Raducu <radu.braga@hogarthww.com>
2020-07-11 19:05:13 +0300

    Add service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { DetailsComponent } from './pages/details/details.component';
 
 @NgModule({
   declarations: [
